9a4565b11be27b8d7a3f0abd7c9a9cefb8a4f9f5
Preeti <preetimpandey11@gmail.com>
2025-08-10 14:14:47 -0400

    Configured initial project setup from start.spring.io

---
+++ src/main/java/com/ba/users/AuctionUsersApplication.java
@@ -0,0 +1,13 @@
+package com.ba.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionUsersApplication.class, args);
+	}
+
+}
